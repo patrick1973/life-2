69cdd139f02d1135c62ea6fdc85b52550f147a97
misteriepie@gmail.com <misteriepie@gmail.com@e19d63b2-37ba-a30f-8433-812836f64864>
2014-01-04 22:27:12 +0000

    Begin gemaakt met database verbindingen. Eerst diverse tools gemaakt.

--- trunk/Life/src/DataAcces/DatabaseBewerkingen.java
+++ trunk/Life/src/DataAcces/DatabaseBewerkingen.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 /**
@@ -11,7 +12,38 @@ import javax.swing.JOptionPane;
  * @author patrick.vanieperen
  */
 public class DatabaseBewerkingen {
-
+    
+    
+    
+    
+/**
+ * Deze methode is gemaakt om een database van de server te verwijderen
+ * indien deze bestaat.
+ * @param naamDatabase, de naam van de database die verwijderd moet worden.
+ */
+public void DropDatabase(String naamDatabase) 
+    {
+        if (checkDBExists(naamDatabase))
+        {
+        try {
+                // open de verbinding
+                Connection con = openSQLConnection();
+                String dropDb = "DROP DATABASE " + naamDatabase;
+                // Execute a query
+                Statement st = con.createStatement();
+                st.execute(dropDb);
+                JOptionPane.showMessageDialog(null, "Database verwijderd", "INFO" , JOptionPane.INFORMATION_MESSAGE);
+            }
+            catch (SQLException ex)
+            {
+                JOptionPane.showMessageDialog(null, "Iets ging er mis->  "+ ex.getMessage(), "ERROR" , JOptionPane.ERROR_MESSAGE);
+            }
+    }else
+    {
+        JOptionPane.showMessageDialog(null, "Database bestaat niet ", "ERROR" , JOptionPane.ERROR_MESSAGE);
+    }
+}
+    
     /**
  * Controleren of een database bestaat op de server
  * @param dbName
@@ -44,6 +76,7 @@ public boolean checkDBExists(String dbName)
     
     /**
  * Open een verbinding met de SQL server
+ * Let op de user en het pass word die zijn hardcoded.
  * @return de open verbinding
  */
 public Connection openSQLConnection()
